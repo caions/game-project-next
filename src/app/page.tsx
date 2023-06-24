a71cebe2ae20011855f5212039d8d026bfb64bae
Caio Nascimento <caionsantos2000@gmail.com>
2023-06-24 17:21:36 -0300

    feat: useApi custom hook

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import axios, { AxiosHeaders } from "axios";
-import Image from "next/image";
 import Card from "@/components/Card";
 import Dropdown from "@/components/DropDown";
+import useApi from "@/hooks/useApi";
 
 export interface Game {
   id: number;
@@ -21,11 +20,30 @@ export interface Game {
 }
 
 export default function Home() {
-  const [games, setGames] = useState<Game[]>([]);
   const [search, setSearch] = useState<string>();
   const [gameGenre, setGameGenre] = useState("");
+  const {
+    data: games,
+    error,
+    isLoading,
+  } = useApi<Game[]>(
+    "https://games-test-api-81e9fb0d564a.herokuapp.com/api/data/"
+  );
 
-  const filteredGames = games.filter((game) => {
+  const sortedGames = games?.sort((a, b) => {
+    const titleA = a.title.toUpperCase();
+    const titleB = b.title.toUpperCase();
+
+    if (titleA < titleB) {
+      return -1;
+    }
+    if (titleA > titleB) {
+      return 1;
+    }
+    return 0;
+  });
+
+  const filteredGames = sortedGames?.filter((game) => {
     if (!gameGenre && !search) {
       return game;
     }
@@ -36,113 +54,96 @@ export default function Home() {
   });
 
   useEffect(() => {
-    if(gameGenre) setSearch("");
+    if (gameGenre) setSearch("");
   }, [gameGenre]);
 
-  useEffect(() => {
-    const headers = new AxiosHeaders({ "dev-email-address": "dev@email.com" });
-    axios
-      .get<Game[]>(
-        "https://games-test-api-81e9fb0d564a.herokuapp.com/api/data/",
-        {
-          headers,
-        }
-      )
-      .then(({ data }) => {
-        const sortedData = data.sort((a, b) => {
-          const titleA = a.title.toUpperCase();
-          const titleB = b.title.toUpperCase();
+  const gamesGenres =
+    games
+      ?.map((game) => game.genre)
+      .filter((genre, index, self) => {
+        return index === self.indexOf(genre);
+      }) || [];
 
-          if (titleA < titleB) {
-            return -1;
-          }
-          if (titleA > titleB) {
-            return 1;
-          }
-          return 0;
-        });
+  const renderBody = () => {
+    if (isLoading) {
+      return <h1>Loading...</h1>
+    }
+    if (error) {
+      return <h1>{error}</h1>
+    }
 
-        setGames(sortedData);
-      })
-      .catch((e) => console.error(e));
-  }, []);
-
-  const gamesGenres = games
-    .map((game) => game.genre)
-    .filter((genre, index, self) => {
-      return index === self.indexOf(genre);
-    });
-
-  return (
-    <>
-      <nav className="flex items-center justify-between flex-wrap bg-teal-500 p-6">
-        <div className="flex items-center flex-shrink-0 text-white mr-6">
-          <svg
-            className="fill-current h-8 w-8 mr-2"
-            width="54"
-            height="54"
-            viewBox="0 0 54 54"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z" />
-          </svg>
-          <span className="font-semibold text-xl tracking-tight">
-            Tailwind CSS
-          </span>
-        </div>
-        <div className="block lg:hidden">
-          <button className="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
+    return (
+      <>
+        <nav className="flex items-center justify-between flex-wrap bg-teal-500 p-6">
+          <div className="flex items-center flex-shrink-0 text-white mr-6">
             <svg
-              className="fill-current h-3 w-3"
-              viewBox="0 0 20 20"
+              className="fill-current h-8 w-8 mr-2"
+              width="54"
+              height="54"
+              viewBox="0 0 54 54"
               xmlns="http://www.w3.org/2000/svg"
             >
-              <title>Menu</title>
-              <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+              <path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z" />
             </svg>
-          </button>
-        </div>
-        <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
-          <div className="text-sm lg:flex-grow">
-            <Dropdown
-              option={["Todos", ...gamesGenres]}
-              selectedValue={(option) => {
-                setGameGenre(option === "Todos" ? "" : option)
-              }
-              }
-              resetFilter={search !== ""}
-            />
+            <span className="font-semibold text-xl tracking-tight">
+              Tailwind CSS
+            </span>
           </div>
-          <div>
-            <div className="mb-4">
-              <input
-                value={search}
-                onChange={(e) => setSearch(e.target.value)}
-                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                id="searchgame"
-                type="text"
-                placeholder="Procurar um jogo"
+          <div className="block lg:hidden">
+            <button className="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
+              <svg
+                className="fill-current h-3 w-3"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <title>Menu</title>
+                <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+              </svg>
+            </button>
+          </div>
+          <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+            <div className="text-sm lg:flex-grow">
+              <Dropdown
+                option={["Todos", ...gamesGenres]}
+                selectedValue={(option) =>
+                  setGameGenre(option === "Todos" ? "" : option)
+                }
+                resetFilter={search !== ""}
               />
             </div>
+            <div>
+              <div className="mb-4">
+                <input
+                  value={search}
+                  onChange={(e) => setSearch(e.target.value)}
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  id="searchgame"
+                  type="text"
+                  placeholder="Procurar um jogo"
+                />
+              </div>
+            </div>
           </div>
-        </div>
-      </nav>
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <div className="grid grid-cols-3 gap-4">
-          {filteredGames.map((game) => (
-            <Card
-              key={game.id}
-              image={game.thumbnail}
-              title={game.title}
-              description={game.short_description}
-              category={game.genre}
-              onClick={() => {
-                setGameGenre(game.genre);
-              }}
-            />
-          ))}
-        </div>
-      </main>
-    </>
-  );
+        </nav>
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+          <div className="grid grid-cols-3 gap-4">
+            {filteredGames?.map((game) => (
+              <Card
+                key={game.id}
+                image={game.thumbnail}
+                title={game.title}
+                description={game.short_description}
+                category={game.genre}
+                onClick={() => {
+                  setGameGenre(game.genre);
+                }}
+              />
+            ))}
+          </div>
+        </main>
+      </>
+    );
+  };
+
+  return renderBody();
 }
---
+++ src/hooks/useApi.ts
@@ -0,0 +1,54 @@
+import { useEffect, useState } from 'react';
+import axios, { AxiosError, AxiosHeaders, AxiosResponse } from 'axios';
+
+type ApiResponse<T> = {
+  data: T | null;
+  error: string | null;
+  isLoading: boolean;
+};
+
+const useApi = <T>(url: string): ApiResponse<T> => {
+  const [response, setResponse] = useState<ApiResponse<T>>({
+    data: null,
+    error: null,
+    isLoading: true
+  });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const headers = new AxiosHeaders({ "dev-email-address": "dev@email.com" });
+        const result: AxiosResponse<T> = await axios.get(url, { headers, timeout: 5000 });
+        setResponse({ data: result.data, error: null, isLoading: false });
+      } catch (error: unknown) {
+        const axiosError = error as AxiosError;
+        const isKnownError = axiosError.response && [500, 502, 503, 504, 507, 508, 509].includes(axiosError.response.status)
+        const isConnectionTimeoutError = axiosError.code === 'ECONNABORTED'
+
+        if (isConnectionTimeoutError) {
+          setResponse({
+            data: null,
+            error: 'O servidor demorou para responder, tente mais tarde.',
+            isLoading: false,
+          });
+          return;
+        }
+        if (isKnownError) {
+          setResponse({
+            data: null,
+            error: 'O servidor falhou em responder, tente recarregar a página.',
+            isLoading: false,
+          });
+          return;
+        }
+        setResponse({ data: null, error: 'Erro desconhecido.', isLoading: false });
+      }
+    };
+
+    fetchData();
+  }, [url]);
+
+  return response;
+};
+
+export default useApi;
