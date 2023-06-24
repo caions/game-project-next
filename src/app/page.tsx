17208419ed9989346875b2bb35501eb8cca1d3fe
Caio Nascimento <caionsantos2000@gmail.com>
2023-06-24 10:44:53 -0300

    feat: fetch games api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,70 @@
+"use client";
+
+import Card from "@/components/Card";
+import axios, { AxiosHeaders } from "axios";
 import Image from "next/image";
+import { useEffect, useState } from "react";
+
+export interface Game {
+  id: number;
+  title: string;
+  thumbnail: string;
+  short_description: string;
+  game_url: string;
+  genre: string;
+  platform: string;
+  publisher: string;
+  developer: string;
+  release_date: string;
+  freetogame_profile_url: string;
+}
 
 export default function Home() {
+  const [games, setGames] = useState<Game[]>([]);
+
+  useEffect(() => {
+    const headers = new AxiosHeaders({ "dev-email-address": "dev@email.com" });
+    axios
+      .get<Game[]>(
+        "https://games-test-api-81e9fb0d564a.herokuapp.com/api/data/",
+        {
+          headers,
+        }
+      )
+      .then(({ data }) => {
+        const sortedData = data.sort((a, b) => {
+          const titleA = a.title.toUpperCase();
+          const titleB = b.title.toUpperCase();
+
+          if (titleA < titleB) {
+            return -1;
+          }
+          if (titleA > titleB) {
+            return 1;
+          }
+          return 0;
+        });
+
+        setGames(sortedData);
+      })
+      .catch((e) => console.error(e));
+  }, []);
+
+  const gamesGenres = games
+  .map((game) => game.genre)
+  .filter((genre, index, self) => {
+    return index === self.indexOf(genre);
+  });
+  console.log(gamesGenres)
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <header className="relative flex place-items-center">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </header>
-      <div className="max-w-sm rounded overflow-hidden shadow-lg">
-      <Image src="" alt='imagem'/>
-        <div className="px-6 py-4">
-          <div className="font-bold text-xl mb-2">Título</div>
-          <p className="text-gray-700 text-base">
-            Descrição
-          </p>
-        </div>
-        <div className="px-6 pt-4 pb-2">
-          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-            #tag
-          </span>
-          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-            #tag2
-          </span>
-          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-            #tag3
-          </span>
-        </div>
+      </header> 
+      <div className="grid grid-cols-3 gap-4">
+        {games.map((game) => (
+          <Card key={game.id} image={game.thumbnail}/>
+        ))}
       </div>
     </main>
   );
