1af789cdf9b0e87697b607f8cf9219b1392f38bf
Caio Nascimento <caionsantos2000@gmail.com>
2023-06-24 12:38:21 -0300

    feat: navbar and search input

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,6 +21,11 @@ export interface Game {
 
 export default function Home() {
   const [games, setGames] = useState<Game[]>([]);
+  const [search, setSearch] = useState<string>();
+
+  const filteredGames = games.filter((g) =>
+    g.title.toLowerCase().includes(search || "")
+  );
 
   useEffect(() => {
     const headers = new AxiosHeaders({ "dev-email-address": "dev@email.com" });
@@ -51,21 +56,76 @@ export default function Home() {
   }, []);
 
   const gamesGenres = games
-  .map((game) => game.genre)
-  .filter((genre, index, self) => {
-    return index === self.indexOf(genre);
-  });
+    .map((game) => game.genre)
+    .filter((genre, index, self) => {
+      return index === self.indexOf(genre);
+    });
   console.log(gamesGenres)
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <header className="relative flex place-items-center">
-      </header> 
-      <div className="grid grid-cols-3 gap-4">
-        {games.map((game) => (
-          <Card key={game.id} image={game.thumbnail}/>
-        ))}
-      </div>
-    </main>
+    <>
+      <nav className="flex items-center justify-between flex-wrap bg-teal-500 p-6">
+        <div className="flex items-center flex-shrink-0 text-white mr-6">
+          <svg
+            className="fill-current h-8 w-8 mr-2"
+            width="54"
+            height="54"
+            viewBox="0 0 54 54"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z" />
+          </svg>
+          <span className="font-semibold text-xl tracking-tight">
+            Tailwind CSS
+          </span>
+        </div>
+        <div className="block lg:hidden">
+          <button className="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
+            <svg
+              className="fill-current h-3 w-3"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <title>Menu</title>
+              <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+            </svg>
+          </button>
+        </div>
+        <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+          <div className="text-sm lg:flex-grow">
+            <a
+              href="#responsive-header"
+              className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white"
+            >
+              Blog
+            </a>
+          </div>
+          <div>
+            <div className="mb-4">
+              <input
+                onChange={(e) => setSearch(e.target.value)}
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="searchgame"
+                type="text"
+                placeholder="Procurar um jogo"
+              />
+            </div>
+          </div>
+        </div>
+      </nav>
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <div className="grid grid-cols-3 gap-4">
+          {filteredGames.map((game) => (
+            <Card
+              key={game.id}
+              image={game.thumbnail}
+              title={game.title}
+              description={game.short_description}
+              category={game.genre}
+            />
+          ))}
+        </div>
+      </main>
+    </>
   );
 }
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -3,30 +3,24 @@ import React from "react";
 
 type Card = {
   image: string;
-  text?: string;
+  title: string;
+  description: string;
+  category: string;
 };
 
-const Card: React.FC<Card> = ({ image, text }) => {
+const Card: React.FC<Card> = ({ image, title, description, category }) => {
   return (
-    <div className="max-w-sm rounded overflow-hidden shadow-lg">
-      <Image src={image} alt="img" width={700} height={700} />
-      <div className="px-6 py-4">
-        <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
-        <p className="text-gray-700 text-base">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus
-          quia, nulla! Maiores et perferendis eaque, exercitationem praesentium
-          nihil.
-        </p>
+    <div className="max-w-sm rounded overflow-hidden shadow-lg flex flex-col place-content-between hover:bg-[#27282e]">
+      <div>
+        <Image src={image} alt="img" width={700} height={700} />
+        <div className="px-6 py-4">
+        <div className="font-bold text-xl mb-2">{title}</div>
+          <p className="text-gray-700 text-base text-[#ffffffa6]">{description}</p>
+        </div>
       </div>
       <div className="px-6 pt-4 pb-2">
         <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-          #photography
-        </span>
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-          #travel
-        </span>
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-          #winter
+          {category}
         </span>
       </div>
     </div>
