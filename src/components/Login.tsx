98d440b516e2dbffa3c7e0462948c1a0544d3d40
Caio Nascimento <caionsantos2000@gmail.com>
2023-07-11 09:05:56 -0300

    refactor: login page

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,17 +1,35 @@
-import { handleSignin, handleSignup } from "@/services/firebaseAuth";
+import { useFirebaseLogin } from "@/hooks/useFirebaseAuth";
 import React, { useState } from "react";
+import { Spinner } from "./Spinner";
+import Link from "next/link";
+import Image from "next/image";
 
 export const Login = () => {
+  const [registerPage, setRegisterPage] = useState(false)
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const { loading, signIn } = useFirebaseLogin(email, password);
+
+
+  const buttonContent = () => {
+    if(loading){
+      return <Spinner/>
+    }
+    if(registerPage){
+      return 'SIGN UP'
+    }
+    return 'LOGIN'
+  }
 
   return (
     <>
       <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-          <img
+          <Image
+            width={0}
+            height={0}
             className="mx-auto h-10 w-auto"
-            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+            src={''}
             alt="Your Company"
           />
           <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-white">
@@ -33,7 +51,7 @@ export const Login = () => {
                   id="email"
                   name="email"
                   type="email"
-                  onChange={(e)=>setEmail(e.target.value)}
+                  onChange={(e) => setEmail(e.target.value)}
                   value={email}
                   required
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
@@ -63,7 +81,7 @@ export const Login = () => {
                   id="password"
                   name="password"
                   type="password"
-                  onChange={(e)=>setPassword(e.target.value)}
+                  onChange={(e) => setPassword(e.target.value)}
                   value={password}
                   required
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
@@ -74,23 +92,23 @@ export const Login = () => {
             <div>
               <button
                 type="button"
-                onClick={()=>handleSignin(email,password)}
-                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                onClick={() => signIn()}
+                disabled={loading}
+                className={`flex w-full justify-center rounded-md bg-indigo-600 disabled:bg-gray-400 disabled:cursor-progress px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600`}
               >
-                Login
+                {buttonContent()}
               </button>
             </div>
           </form>
 
           <p className="mt-10 text-center text-sm text-gray-500">
-            Don&apos;t have an account?
-            <a
-              href="#"
+            Don&apos;t have an account?{" "}
+            <button
+              onClick={()=>setRegisterPage(!registerPage)}
               className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
             >
-              {" "}
               Register
-            </a>
+            </button>
           </p>
         </div>
       </div>
